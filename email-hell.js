88e8fff0666830eb7bc8ee115bbd176c12ee3da7
Edward Gao <egao@sent.com>
2017-02-11 10:09:01 -1000

    super short one array function

--- email-hell.js
+++ email-hell.js
@@ -1,19 +1,7 @@
 const log = require('./email_log').emails;
 
-var report = ({addr, num}) =>
-	console.log(`${addr} received ${num} repeated emails`);
 var repeats = log
 	.reduce((prev, {email}) => {
-		if(prev.some(({addr}) => addr === email)) {
-			return prev.map((elem) => {
-				if(elem.addr === email) elem.num += 1;
-				return elem;
-			});
-		}else{
-			prev.push({addr: email, num: 1});
-			return prev;
-		}
-	}, [])
-	.filter(({num}) => num > 1)
-	.map(user => report(user));
-
+		prev[email] = (prev[email] | 0) + 1;
+		return prev;
+	}, {});
